ce1f8281851062619b4d146c35d6be520637694b
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-11 14:41:28 +0900

    Scanner nextInt()사용해서 간단한 자판기 만들기 + 가격 추가 + 잔돈 + 부족한 금액 추가 메세지

--- src/Main.java
+++ src/Main.java
@@ -9,31 +9,39 @@ public class Main {
         System.out.println("1. 멜론소다 : 1000원");
         System.out.println("2. 콜라 : 2000원");
         System.out.println("3. 사이다 : 3000원");
-        System.out.println("4. 환타 : 5000원");
-        System.out.println("5. 물 : 6000원");
+        System.out.println("4. 환타 : 4000원");
+        System.out.println("5. 물 : 5000원");
 
         int choice = scanner.nextInt();
+        int price = 0;
+
         String item = "";
+
 
         switch (choice) {
             case 1:
                 item = "멜론소다";
+                price = 1000;
                 break;
 
             case 2:
                 item = "콜라";
+                price = 2000;
                 break;
 
             case 3:
                 item = "사이다";
+                price = 3000;
                 break;
 
             case 4:
                 item = "환타";
+                price = 4000;
                 break;
 
             case 5:
                 item = "물";
+                price =5000;
                 break;
 
             default:
@@ -42,7 +50,17 @@ public class Main {
 
 
         }
-        System.out.println(item);
+        System.out.println(item + "을(를) 선택하셨습니다. 금액을 넣어주세요 : ");
+        int money = scanner.nextInt();
+
+        while (money < price) {
+            System.out.println("금액이 부족합니다. 부족한 금액을 추가해 주시길 바랍니다. 현재 투입된 금액은 : " + money + "입니다.");
+            int add = scanner.nextInt();
+            money += add;
+        } if (money > price) {
+            int change = money - price;
+            System.out.println(item  + "을(를) 드립니다. 잔돈은 " + change + "원 입니다.");
+        }
 
     }
 }
