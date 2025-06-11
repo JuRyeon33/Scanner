ecf4676c592993cb621a4be4a97bd27e1fa8a2e8
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-11 14:12:48 +0900

    Scanner nextInt()사용해서 간단한 자판기 만들기 + 가격 추가

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+
+        System.out.println("-----자판기 메뉴-----");
+        System.out.println("1. 멜론소다 : 1000원");
+        System.out.println("2. 콜라 : 2000원");
+        System.out.println("3. 사이다 : 3000원");
+        System.out.println("4. 환타 : 5000원");
+        System.out.println("5. 물 : 6000원");
+
         int choice = scanner.nextInt();
         String item = "";
 
@@ -18,6 +26,14 @@ public class Main {
 
             case 3:
                 item = "사이다";
+                break;
+
+            case 4:
+                item = "환타";
+                break;
+
+            case 5:
+                item = "물";
                 break;
 
             default:
